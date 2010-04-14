302a84434c5d574d105ea24a760843d56b674ee7
Daniel Fuentes B. <dbfuentes@gmail.com>
2010-04-14 18:21:23 -0400

    working on the option to compare hashes: * src/pysum.glade: add a tab (gtk-notebook) for compare hashes * src/pysum.py: partially added support to compare hashes

--- src/pysum.py
+++ src/pysum.py
@@ -22,9 +22,9 @@
 ########### START EDIT HERE ###########
 
 # Directory with the files (*.glade)
-resources_dir = "/usr/share/pysum"
+RESOURCES_DIR = "/usr/share/pysum"
 # Directory with the img. (icons)
-img_dir = "/usr/share/pysum"
+IMG_DIR = "/usr/share/pysum"
 
 ########### STOP EDIT HERE ############
 
@@ -32,21 +32,19 @@ img_dir = "/usr/share/pysum"
 # Informacion del programa que se modifica con cierta frecuencia
 # (para no escribir tanto)
 
-__version__ = "0.4"
-
-authors = "Daniel Fuentes Barría <dbfuentes@gmail.com>"
-website = "http://pysum.berlios.de/"
-license = "This program is free software; you can redistribute \
-it and/or modify it under the terms of the GNU General Public License as \
-published by the Free Software Foundation; either version 2 of the \
-License, or (at your option) any later version. \n\nThis program is \
+__version__ = "0.5 alpha"
+AUTHOR = "Daniel Fuentes Barría <dbfuentes@gmail.com>"
+WEBSITE = "http://pysum.berlios.de/"
+LICENCE = "This program is free software; you can redistribute \
+it and/or modify it under the terms of the GNU General Public License \
+as published by the Free Software Foundation; either version 2 of the \
+License, or (at your option) any later version.\n\nThis program is \
 distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; \
 without even the implied warranty of MERCHANTABILITY or FITNESS FOR A \
-PARTICULAR PURPOSE. \
-See the GNU General Public License for more details. \n\nYou should have \
-received a copy of the GNU General Public License along with this program; \
-if not, write to the Free Software Foundation, Inc., 51 Franklin Street, \
-Fifth Floor, Boston, MA 02110-1301, USA."
+PARTICULAR PURPOSE. See the GNU General Public License for more details.\
+\n\nYou should have received a copy of the GNU General Public License \
+along with this program; if not, write to the Free Software Foundation, \
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA."
 
 # ---------------------------------------------------------------------
 # Importamos los modulos necesarios
@@ -81,8 +79,7 @@ gtk.glade.textdomain("pysum")
 
 
 # Las siguientes Clases/funciones calculan los hash del un archivo.
-# Es importante que el .read() se realice por partes para no
-# llenar la memoria
+# El .read() hay que realizarlo por partes para no llenar la memoria
 
 
 class hashfile():
@@ -185,8 +182,8 @@ def valor_combobox(combobox):
     return model[activo][0]
 
 
-# Interfaz grafica (gtk-glade)
-# Clase para el Loop principal (de la interfaz grafica)
+# ===================================================================
+# Clase para el Loop principal de la interfaz grafica (gtk-glade)
 
 
 class MainGui:
@@ -198,13 +195,15 @@ class MainGui:
         if os.path.exists("pysum.glade"):
             self.widgets = gtk.glade.XML("pysum.glade")
         else:
-            self.widgets = gtk.glade.XML(os.path.join(resources_dir,
+            self.widgets = gtk.glade.XML(os.path.join(RESOURCES_DIR,
             "pysum.glade"))
 
         # Creamos un diccionario con los manejadores definidos en glade
         # y sus respectivas llamadas.
         signals = {"on_buttonopen1_clicked": self.on_buttonopen1_clicked,
                    "on_buttonok1_clicked": self.on_buttonok1_clicked,
+                   "on_buttonopen2_clicked": self.on_buttonopen2_clicked,
+                   "on_buttonok2_clicked": self.on_buttonok2_clicked,
                    "on_about1_activate": self.on_about1_activate,
                    "gtk_main_quit": gtk.main_quit}
 
@@ -212,21 +211,25 @@ class MainGui:
         self.widgets.signal_autoconnect(signals)
 
         ## Del archivo glade obtenemos los widgets a usar
-        # estas widgets son de las pestaña para obtener hash
+        # estas widgets son de las pestañas para obtener hash
         self.entry1 = self.widgets.get_widget("entry1")
         self.textview1 = self.widgets.get_widget("textview1")
+        # segunda pestaña (la uasada para comparar)
+        self.entry2 = self.widgets.get_widget("entry2")
+        self.entry3 = self.widgets.get_widget("entry3")
 
         # En el ComboBox hay que seleccionar por defecto la primera opcion
         self.combobox1 = self.widgets.get_widget("combobox1")
-        self.combobox1.set_active(1) # Fijamos el segundo elemento de la lista
-
+        self.combobox1.set_active(1)
+        self.combobox2 = self.widgets.get_widget("combobox2")
+        self.combobox2.set_active(1)
 
     # Similar al .glade, hay que determinar donde esta el icono del programa
     # primero asumimos que esta en el directorio de las fuentes (trunk/img/)
         self.icono = os.path.join(os.pardir, "img", "pysum.png")
         if os.path.exists(self.icono) == False:
-            # si no esta alli, cambiamos la ruta a la definida en img_dir
-            self.icono = os.path.join(img_dir, "pysum.png")
+            # si no esta alli, cambiamos la ruta a la definida en IMG_DIR
+            self.icono = os.path.join(IMG_DIR, "pysum.png")
 
         # Ahora le agregamos el icono a la ventana
         self.mainwindow = self.widgets.get_widget("mainwindow")
@@ -234,7 +237,6 @@ class MainGui:
             self.mainwindow.set_icon_from_file(self.icono)
         except:
             print "Error: no se puede cargar el icono: %s" % (self.icono)
-
 
     # De aqui en adelante comienza las acciones propias del programa
     # Como las ventanas especiales y las acciones a realizar
@@ -249,7 +251,7 @@ class MainGui:
         file_open = gtk.FileChooserDialog(title=_("Select a file"),
                     action=gtk.FILE_CHOOSER_ACTION_OPEN,
                     buttons=dialog_buttons)
-        resultado = "" # Aquí almacenamos la ruta del archivo
+        resultado = ""  # Aquí almacenamos la ruta del archivo
         if file_open.run() == gtk.RESPONSE_OK:
             resultado = file_open.get_filename()
         file_open.destroy()
@@ -277,23 +279,21 @@ class MainGui:
 verify md5 and other checksum"))
         about.set_copyright("Copyright © 2008-2010 Daniel Fuentes B.")
 
-        def openHomePage(widget, url, url2): # Para abrir el sitio
+        def openHomePage(widget, url, url2):  # Para abrir el sitio
             import webbrowser
             webbrowser.open_new(url)
 
-        gtk.about_dialog_set_url_hook(openHomePage, website)
+        gtk.about_dialog_set_url_hook(openHomePage, WEBSITE)
         try:
             about.set_logo(gtk.gdk.pixbuf_new_from_file(self.icono))
         except:
             print "Error: no se puede cargar el icono: %s" % (self.icono)
-        about.set_website(website)
-        about.set_authors([authors])
-        about.set_license(license)
-        about.set_wrap_license(True) # Adapta el texto a la ventana
-
+        about.set_website(WEBSITE)
+        about.set_authors([AUTHOR])
+        about.set_license(LICENCE)
+        about.set_wrap_license(True)  # Adapta el texto a la ventana
         about.run()
         about.destroy()
-
 
 # Declaramos las acciones a realizar (menus, botones, etc.):
 
@@ -302,15 +302,15 @@ verify md5 and other checksum"))
         "Open the About windows"
         self.about_info()
 
-    # Definimos las acciones de los botones #
-    # Pestaña que obtiene los hash
+    # --------------------------------------------------------------------
+    # Pestaña que obtiene el hash de un archivo
 
-    def on_buttonopen1_clicked(self, widget): # Boton abrir
+    def on_buttonopen1_clicked(self, widget):  # Boton abrir 1
         "Called when the user wants to open a file"
         ruta_archivo = self.file_browse()
         self.entry1.set_text(ruta_archivo)
 
-    def on_buttonok1_clicked(self, widget): #Boton aceptar, para calcular hash
+    def on_buttonok1_clicked(self, widget):  # Boton ok para calcular hash
         "This button generate the hash"
         text_buffer = gtk.TextBuffer()
         # Comprobamos la opcion elegida en el ComboBox
@@ -318,8 +318,7 @@ verify md5 and other checksum"))
         # Se obtiene la ruta del archivo desde la entrada y crea un buffer
         texto_entry1 = self.entry1.get_text()
         archivo = hashfile(texto_entry1)
-        # Se intenta obtener el hash, dependiendo de la opcion escogida en
-        # el ComboBox hay que obtener el hash correcto (md5, sha-1, etc)
+        # Se intenta obtener el hash, dependiendo de la opcion escogida
         try:
             if combobox_selec == "md5":
                 text_buffer.set_text(str(archivo.getmd5()))
@@ -343,6 +342,46 @@ verify md5 and other checksum"))
         # Se muestra el buffer (hash obtenido) en textview
         self.textview1.set_buffer(text_buffer)
 
+    # --------------------------------------------------------------------
+    # Pestaña que compara el hash de un archivo con un valor experado
+
+    def on_buttonopen2_clicked(self, widget):  # Boton abrir 2
+        "Called when the user wants to open a file"
+        ruta_archivo = self.file_browse()
+        self.entry2.set_text(ruta_archivo)
+
+    def on_buttonok2_clicked(self, widget):
+        combobox_selec = valor_combobox(self.combobox2)
+        texto_entry2 = self.entry2.get_text()
+        archivo = hashfile(texto_entry2)
+        try:
+            if combobox_selec == "md5":
+                resultado_hash = str(archivo.getmd5())
+            elif combobox_selec == "sha1":
+                resultado_hash = str(archivo.getsha1())
+            elif combobox_selec == "sha224":
+                resultado_hash = str(archivo.getsha224())
+            elif combobox_selec == "sha256":
+                resultado_hash = str(archivo.getsha256())
+            elif combobox_selec == "sha384":
+                resultado_hash = str(archivo.getsha384())
+            elif combobox_selec == "sha512":
+                resultado_hash = str(archivo.getsha512())
+            elif combobox_selec == "CRC32":
+                resultado_hash = str(archivo.getcrc32())
+        except:
+            if (len(texto_entry2) == 0):
+                self.error(_("Please choose a file"))
+            else:
+                self.error(_("Can't open the file:") + texto_entry2)
+        resultado_hash = resultado_hash.lower()
+        hash_esperado = str(self.entry3.get_text())
+        hash_esperado = hash_esperado.lower()
+        if resultado_hash == hash_esperado:
+            self.error(_("%s Checksums are the same" % (combobox_selec)))
+        else:
+            self.error(_("Checksums diferent\nFile: %s\nExpected: %s" % (
+                resultado_hash, hash_esperado)))
 
 if __name__ == "__main__":
     MainGui()
